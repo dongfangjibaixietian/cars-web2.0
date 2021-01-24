627cdd25633bfdb2a1bc27ada7f34f9c01ae07d9
unknown <dongdongfangjibaixietian>
2021-01-25 00:50:27 +0800

    swiper

---
+++ src/components/carslist/index.vue
@@ -0,0 +1,167 @@
+<template>
+    <section class="cars-item">
+          <header>
+            <h4 class="cars-header">
+              <img
+                src="../../assets/images/cars-logo.png"
+                alt="Mustang 2019款"
+              />
+              <span>Mustang 2019款</span>
+            </h4>
+            <p class="cars-name">新能源汽车 5座</p>
+          </header>
+          <div class="cars-content">
+            <div class="info">
+              <div>
+                <h4 class="cars-number">粤 B745N8</h4>
+                <div>
+                  <ul class="cars-electric">
+                    <li class="actice"></li>
+                    <li class="actice"></li>
+                    <li class="actice"></li>
+                    <li class="actice"></li>
+                    <li class="actice"></li>
+                    <li class="actice"></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                  </ul>
+                  <p class="distance">
+                    <sub>约</sub>
+                    <strong>600</strong>
+                    <sub>KM</sub>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <img src="../../assets/images/pic001.jpg" alt="" />
+          </div>
+          <footer>
+            <a href="" class="cars-parking">某某停车场</a>
+          </footer>
+        </section>
+</template>
+
+<script>
+export default {
+    name: "CarsList"
+}
+</script>
+
+<style lang="scss" scoped>
+
+.cars-item {
+  width: 505px;
+  height: 257px;
+  background-color: #fff;
+  margin: auto;
+  box-sizing: border-box;
+  padding: 30px 30px 0px 30px;
+  border-radius: 24px;
+  box-shadow: 0 0 18px 0 rgba(0, 0, 0, 0.2);
+}
+
+header {
+  display: flex;
+  h4,
+  p {
+    flex: 1;
+  }
+}
+
+.cars-header {
+  img {
+    display: inline-block;
+    height: 34px;
+    vertical-align: middle;
+    margin-right: 10px;
+  }
+}
+
+.cars-name {
+  font-size: 16px;
+  opacity: 0.5;
+  text-align: center;
+  line-height: 34px;
+}
+
+.cars-content {
+  position: relative;
+  margin: 11px 0 7px;
+  &:after {
+    clear: both;
+    content: ".";
+    display: block;
+    height: 0;
+    line-height: 0;
+    overflow: hidden;
+  }
+  img {
+    float: right;
+    height: 120px;
+  }
+  .info {
+    display: flex;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+  }
+  .cars-number {
+    font-size: 18px;
+  }
+  .cars-electric {
+    padding-top: 10px;
+    display: inline-block;
+    li {
+      display: inline-block;
+      width: 7px;
+      height: 24px;
+      margin-right: 3px;
+      background-color: #e1e1e1;
+      border-radius: 100px;
+      &.actice {
+        background: linear-gradient(#17abfa, #108dd9);
+      }
+    }
+  }
+  .distance {
+    margin-left: 5px;
+    display: inline-block;
+    strong {
+      margin: 0 5px;
+      font-size: 22px;
+    }
+    sub {
+      position: relative;
+      top: -2px;
+    }
+    sub:nth-child(1) {
+      opacity: 0.8;
+    }
+  }
+}
+.cars-parking {
+  position: relative;
+  display: block;
+  border-top: 1px solid #e1e1e1;
+  color: $color-main;
+  opacity: 0.8;
+  line-height: 53px;
+  &:after {
+    content: "";
+    position: absolute;
+    right: 0px;
+    top: 50%;
+    width: 8px;
+    height: 8px;
+    margin-top: -8px;
+    border-top: 1px solid #e1e1e1;
+    border-right: 1px solid #e1e1e1;
+    transform: rotate(45deg);
+  }
+}
+</style>
--- src/views/cars/index.vue
+++ src/views/cars/index.vue
@@ -1,50 +1,35 @@
 <template>
   <div class="cars-wrap">
-    <section class="cars-item">
-      <header>
-        <h4 class="cars-header">
-          <img src="../../assets/images/cars-logo.png" alt="Mustang 2019款" />
-          <span>Mustang 2019款</span>
-        </h4>
-        <p class="cars-name">新能源汽车 5座</p>
-      </header>
-      <div class="cars-content">
-        <div class="info">
-          <div>
-            <h4 class="cars-number">粤 B745N8</h4>
-            <div>
-              <ul class="cars-electric">
-                <li class="actice"></li>
-                <li class="actice"></li>
-                <li class="actice"></li>
-                <li class="actice"></li>
-                <li class="actice"></li>
-                <li class="actice"></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-              </ul>
-              <p class="distance">
-                <sub>约</sub>
-                <strong>600</strong>
-                <sub>KM</sub>
-              </p>
-            </div>
-          </div>
-        </div>
-        <img src="../../assets/images/pic001.jpg" alt="" />
-      </div>
-      <footer></footer>
-    </section>
+    <div class="cars-swiper-wrap">
+      <swiper class="swiper" :options="swiperOption">
+        <swiper-slide><CarsList /></swiper-slide>
+        <swiper-slide><CarsList /></swiper-slide>
+        <swiper-slide><CarsList /></swiper-slide>
+      </swiper>
+      <div class="swiper-button-prev" slot="button-prev"></div>
+      <div class="swiper-button-next" slot="button-next"></div>
+    </div>
   </div>
 </template>
 
 <script>
+import CarsList from "../../components/carslist/index";
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+import "swiper/swiper-bundle.css";
 export default {
   name: "Cars",
+  components: {
+    Swiper,
+    SwiperSlide,
+    CarsList,
+  },
   data() {
-    return {};
+    return {
+      swiperOption: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+    };
   },
   methods: {
     // changeUse() {
@@ -64,98 +49,7 @@ export default {
   bottom: 94px;
   z-index: 11;
 }
-
-.cars-item {
-  width: 505px;
-  height: 257px;
-  background-color: #fff;
-  margin: auto;
-  box-sizing: border-box;
-  padding: 30px;
-  border-radius: 24px;
-  box-shadow: 0 0 18px 0 rgba(0, 0, 0, 0.2);
-}
-
-header {
-  display: flex;
-  h4,
-  p {
-    flex: 1;
-  }
-}
-
-.cars-header {
-  img {
-    display: inline-block;
-    height: 34px;
-    vertical-align: middle;
-    margin-right: 10px;
-  }
-}
-
-.cars-name {
-  font-size: 16px;
-  opacity: 0.5;
-  text-align: center;
-  line-height: 34px;
-}
-
-.cars-content {
-  position: relative;
-  margin: 11px 0 7px;
-  &:after {
-        clear: both;
-        content: '.';
-        display: block;
-        height: 0;
-        line-height: 0;
-        overflow: hidden;
-    }
-  img {
-    float: right;
-    height: 120px;
-  }
-  .info {
-    display: flex;
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    align-items: center;
-  }
-  .cars-number {
-    font-size: 18px;
-  }
-  .cars-electric { 
-    padding-top: 10px;
-    display: inline-block;
-    li {
-      display: inline-block;
-      width: 7px;
-      height: 24px;
-      margin-right: 3px;
-      background-color: #e1e1e1;
-      border-radius: 100px;
-      &.actice {
-        background: linear-gradient(#17aBfa,#108dd9);
-      }
-    }
-  }
-  .distance {
-    margin-left: 5px;
-    display: inline-block;
-    strong {
-      margin: 0 5px;
-      font-size: 22px;
-    }
-    sub {
-      position: relative;
-      top: -2px;
-    }
-    sub:nth-child(1) {
-      opacity: .8;
-    }
-  }
+.cars-swiper-wrap {
+  padding: 0px 155px;
 }
 </style>
